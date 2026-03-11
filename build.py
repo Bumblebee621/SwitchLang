c3649ef9da343a5d1d89dc482b26646e93bc149c
Bumblebee621 <ariel25621@gmail.com>
2026-03-11 21:04:11 +0200

    Add build.py script for automated PyInstaller bundling

---
+++ build.py
@@ -0,0 +1,14 @@
+import PyInstaller.__main__
+import os
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+
+PyInstaller.__main__.run([
+    'main.py',
+    '--name=SwitchLang',
+    '--noconsole',
+    '--onefile',
+    f'--add-data={os.path.join(APP_DIR, "data")};data',
+    f'--add-data={os.path.join(APP_DIR, "ui")};ui',
+    '--clean'
+])
