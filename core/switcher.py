dcad01307376ea47fb0cc1f9f4cbde696f71f722
Bumblebee621 <ariel25621@gmail.com>
2026-03-14 14:16:40 +0200

    FIxed a fatal bug that was introduced in the last few commits

--- core/switcher.py
+++ core/switcher.py
@@ -35,6 +35,16 @@ HKL_EN_US = 0x04090409
 HKL_HE = 0x040D040D
 
 
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = [
+        ('dx', wintypes.LONG),
+        ('dy', wintypes.LONG),
+        ('mouseData', wintypes.DWORD),
+        ('dwFlags', wintypes.DWORD),
+        ('time', wintypes.DWORD),
+        ('dwExtraInfo', ctypes.POINTER(ctypes.c_ulong)),
+    ]
+
 class KEYBDINPUT(ctypes.Structure):
     _fields_ = [
         ('wVk', wintypes.WORD),
@@ -44,10 +54,19 @@ class KEYBDINPUT(ctypes.Structure):
         ('dwExtraInfo', ctypes.POINTER(ctypes.c_ulong)),
     ]
 
+class HARDWAREINPUT(ctypes.Structure):
+    _fields_ = [
+        ('uMsg', wintypes.DWORD),
+        ('wParamL', wintypes.WORD),
+        ('wParamH', wintypes.WORD),
+    ]
+
 class INPUT(ctypes.Structure):
     class _INPUT_UNION(ctypes.Union):
         _fields_ = [
             ('ki', KEYBDINPUT),
+            ('mi', MOUSEINPUT),
+            ('hi', HARDWAREINPUT),
         ]
 
     _fields_ = [
@@ -56,6 +75,10 @@ class INPUT(ctypes.Structure):
     ]
 
 _EXTRA_INFO = ctypes.pointer(ctypes.c_ulong(0))
+
+# Set argtypes and restype for critical user32 functions
+user32.SendInput.argtypes = [wintypes.UINT, ctypes.POINTER(INPUT), ctypes.c_int]
+user32.SendInput.restype = wintypes.UINT
 
 
 def _make_key_input(vk=0, scan=0, flags=0):
